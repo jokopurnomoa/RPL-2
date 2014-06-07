f7879b7000e4ff5ebc3e1cc00af6aa199ce54ac8
jokopurnomoa <jokopurnomoa@gmail.com>
2014-06-07 19:53:58 +0700

    Penambahan script MainMenu.cs

---
+++ Game-2d/Beruang/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		if (this.gameObject.name == "play") {
+			Debug.Log("Button play clicked!");
+			Application.LoadLevel("main_scene");
+		} else if (this.gameObject.name == "score") {
+			
+		} else if (this.gameObject.name == "help") {
+			
+		} else if (this.gameObject.name == "exit") {
+			
+		}
+	}
+}
